6f5c7192d863aa1d9d8da5846140e2b3b0f1e029
Ilmoretto <morete.alencar@gmail.com>
2026-06-10 23:38:46 -0400

    feat: implementação completa do módulo de Professores
    - Limpeza das marcações redundantes [Column] e [Table] da entidade Professor.
    - Criação dos DTOs de entrada e saída (ProfessorDto, ProfessorUpdateDto, ProfessorResponseDto) com validações de Data Annotations.
    - Implementação de Filtro Paginado (ProfessorFilter) com buscas por CPF, Especialidade e Status.
    - Criação do Service com lógica de negócio e CRUD (ProfessorService).
    - Criação da Controller versionada com os endpoints da API (ProfessorController).
    - Configuração do mapeamento no AutoMapper (ProfessorProfile).
    - Injeção das novas classes no container de dependência do Program.cs.
    - Limpeza de arquivos .gitkeep que não são mais necessários.

--- Models/Professor.cs
+++ Models/Professor.cs
@@ -4,44 +4,31 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AppEscolaDeMusica.Models
 {
-    [Table("professor"), PrimaryKey(nameof(Id))]
     public class Professor
     {
-        [Column("id")]
         public int Id { get; set; }
 
-        [Column("nome")]
         public required string Nome { get; set; }
 
-        [Column("cpf")]
         public required string Cpf { get; set; }
 
-        [Column("rg")]
         public required string Rg { get; set; }
 
-        [Column("email")]
         public string Email { get; set; }
 
-        [Column("telefone")]
         public required string Telefone { get; set; }
 
-        [Column("data_admissao")]
         public required DateOnly DataAdmissao { get; set; }
 
-        [Column("data_demissao")]
         public DateOnly? DataDemissao { get; set; }
 
-        [Column("status_prof")]
         public StatusProfessorEnum StatusProf { get; set; }
 
-        [Column("especialidade")]
         public required string Especialidade { get; set; }
 
-        [Column("valor_hora_aula")]
         public required decimal ValorHoraAula { get; set; }
 
-        public ICollection<Ministra> Ministras { get; set; } = new List<Ministra>();
-        
+        public ICollection<Ministra> Ministras { get; set; } = new List<Ministra>();        
         public ICollection<DisponibilidadeProfessor> Disponibilidades { get; set; } = new List<DisponibilidadeProfessor>();
     }
 }
---
+++ Profiles/ProfessorProfile.cs
@@ -0,0 +1,18 @@
+﻿using AppEscolaDeMusica.Dtos.Professores;
+using AppEscolaDeMusica.Models;
+using AutoMapper;
+
+namespace AppEscolaDeMusica.Profiles
+{
+    public class ProfessorProfile : Profile
+    {
+        public ProfessorProfile()
+        {
+            CreateMap<ProfessorDto, Professor>();
+            CreateMap<ProfessorUpdateDto, Professor>()
+                .ForAllMembers(opts => opts.Condition((src, dest, srcMember) => srcMember != null));
+
+            CreateMap<Professor, ProfessorResponseDto>();
+        }
+    }
+}
